959d6ed0316fcb23e3f166329018bba89d2f513d
Jackson Gunther <jackson.gunther@yahoo.com>
2021-10-18 21:48:51 -0600

    gitignore

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def in2post(expr):
     }
     op_stack = Stack()
     postfix_list = []
-    token_list = expr[0].split()
-    print(f"infix: {expr[0]}")
+    token_list = expr[0]
+    print(f"infix: {token_list}")
     for token in token_list:
         if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
             postfix_list.append(token)
@@ -26,7 +26,7 @@ def in2post(expr):
                 postfix_list.append(top_token)
                 top_token = op_stack.pop()
         else:
-            while (not op_stack.is_empty()) and (prec[op_stack.peek()] >= prec[token]):
+            while (not op_stack.is_empty()) and (prec[op_stack.top()] >= prec[token]):
                 postfix_list.append(op_stack.pop())
             op_stack.push(token)
     # print(dir(op_stack))
@@ -44,7 +44,7 @@ def main():
     for i in expression_list:
         expression = i.strip().split(',')
         #expression_stack.push(expression)
-        print(in2post(expression))
+        in2post(expression)
     #print(expression_stack)
 
 if __name__ == "__main__":
